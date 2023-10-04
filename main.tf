67aebc386a29cb96abd3fa6a9ff9faf242736efa
Ashrafmiya <140480541+Ashrafmiya@users.noreply.github.com>
2023-10-04 10:19:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
-required_providers {
+terraform {
+  required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.19.0"
     }
   }
 }
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 provider "aws" {
-  access_key= "var.accesskey"
-  secret_key= "var.secretkey"
-  region     = "us-east-1"
+access_key = var.AWS_ACCESS_KEY
+secret_key = var.AWS_SECRET_KEY
+region = "ap-south-1"
 }
+ 
 resource "aws_instance" "myec2" {
-  ami           = "ami-03a6eaae9938c858c"
+  ami           = "ami-067c21fb1979f0b27"
   instance_type = "t2.micro"
   vpc_security_group_ids=[aws_security_group.web-sg.id]
   key_name="tf-key-pair"
-
+ 
+ 
 tags={
  Name="web-server"
 }
@@ -36,8 +41,9 @@ ingress {
 protocol="tcp"
 cidr_blocks= ["0.0.0.0/0"]
 }
-
- ingress {
+ 
+ 
+ingress {
  from_port=22
  to_port=22
 protocol="tcp"
